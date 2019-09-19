a79c1ac763b1fef6b30d766cddd16b6a154cb103
Jeremy Sparagon <sparagon@grinnell.edu>
2019-09-18 23:35:13 -0500

    updated tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'subpage/what'
+  get 'homepage/home'
   get "/", to: 'homepage#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'homepage#home'
